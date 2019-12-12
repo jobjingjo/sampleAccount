33153b8c23b1ecea9abad41c3e02d1441afb0dca
Kaiwanyawit Chawankul <Kaiwanyawit.C@draycir.com>
2019-12-12 15:23:20 +0700

    get IBAN using Puppeteer

---
+++ test/sampleAccount.Tests/AccountServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using sampleAccount.Abstract;
+using sampleAccount.Services;
+
+namespace sampleAccount.Tests
+{
+    [TestClass]
+    public class AccountServiceTests
+    {
+        private AccountService _target;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _target = new AccountService();
+        }
+
+        [TestMethod]
+        public async Task GetIBAN()
+        {
+            var result = await _target.GetIBAN();
+            Assert.AreNotEqual(0,result);
+        }
+    }
+}
