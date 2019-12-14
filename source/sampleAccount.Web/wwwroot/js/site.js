ae359d0cb7c35f79de017680629c2121f8f97aad
Kaiwanyawit Chawankul <kaiwanyawit.c@draycir.com>
2019-12-14 23:44:05 +0700

    show only one toast

--- source/sampleAccount.Web/wwwroot/js/site.js
+++ source/sampleAccount.Web/wwwroot/js/site.js
@@ -5,7 +5,11 @@
 (function () {
     "use strict";
     var app = angular.module('myApp', []);
-
+    app.run(function ($rootScope) {   
+        $rootScope.balance = "mock";
+        $rootScope.errorMessage = "mock";
+        $rootScope.error = false;
+    });
 }());
 
 (function () {
@@ -53,11 +57,11 @@
                     if (response.status === 200) {
                         deferred.resolve(response.data);
                     } else {
-                        deferred.reject(response);
+                        deferred.reject("Not enough balance");
                     }
                 },
                     function myError(response) {
-                        deferred.reject(response);
+                        deferred.reject("Please try again");
                     });
 
             return deferred.promise;
@@ -72,11 +76,11 @@
                     if (response.status === 200) {
                         deferred.resolve(response.data);
                     } else {
-                        deferred.reject(response);
+                        deferred.reject("Account not found");
                     }
                 },
                     function myError(response) {
-                        deferred.reject(response);
+                        deferred.reject("Please try again");
                     });
 
             return deferred.promise;
@@ -91,11 +95,11 @@
                     if (response.status === 200) {
                         deferred.resolve(response.data);
                     } else {
-                        deferred.reject(response);
+                        deferred.reject("Can not Transfer");
                     }
                 },
                     function myError(response) {
-                        deferred.reject(response);
+                        deferred.reject("Please try again");
                     });
 
             return deferred.promise;
@@ -112,7 +116,7 @@
                     if (response.status === 200) {
                         deferred.resolve(response.data);
                     } else {
-                        deferred.reject(response);
+                        deferred.reject(response.data);
                     }
                 },
                     function myError(response) {
@@ -128,23 +132,26 @@
 
 (function () {
     "use strict";
-     function withdrawController($scope, accountService, $timeout, $window) {
+    function withdrawController($rootScope, $scope, accountService, $timeout, $window) {
 
          $scope.doWithdraw = function () {
              accountService.withdraw($scope.amount).then(x => {
-                 $scope.balance = x;
-                 console.log(x);
-                 //$("#overlay").show();
+                 $rootScope.balance = x;
+                 $rootScope.error = false;
+                 console.log(x);                 
                  $('.toast').toast('show');
 
                  $timeout(function () { $window.location.href = 'https://localhost:44318/Account/Index'; }, 1000);
 
              }).catch(err => {
+                 $rootScope.error = true;
+                 $rootScope.errorMessage = err;
+                 $('.toast').toast('show');
                  console.log(err);
              });
          };
     }
-    withdrawController.$inject = ['$scope', 'accountService', '$timeout', '$window'];
+    withdrawController.$inject = ['$rootScope','$scope', 'accountService', '$timeout', '$window'];
     angular.module('myApp').controller('withdrawController', withdrawController);
 
 }());
@@ -152,60 +159,68 @@
 
 (function () {
     "use strict";
-    function depositController($scope, accountService, $timeout, $window) {
+    function depositController($rootScope,$scope, accountService, $timeout, $window) {
 
         $scope.doDeposit = function () {
             accountService.deposit($scope.amount).then(x => {
-                $scope.balance = x;
+                $rootScope.balance = x;
+                $rootScope.error = false;
                 console.log(x);
-                //$("#overlay").show();
                 $('.toast').toast('show');
 
                 $timeout(function () { $window.location.href = 'https://localhost:44318/Account/Index'; }, 1000);
 
             }).catch(err => {
+                $rootScope.error = true;
+                $rootScope.errorMessage = err;
+                $('.toast').toast('show');
                 console.log(err);
             });
         };
 
 
     }
-    depositController.$inject = ['$scope', 'accountService', '$timeout', '$window'];
+    depositController.$inject = ['$rootScope','$scope', 'accountService', '$timeout', '$window'];
     angular.module('myApp').controller('depositController', depositController);
 
 }());
 
 (function () {
     "use strict";
-    function transferController($scope, accountService, $window, $timeout) {
+    function transferController($rootScope, $scope, accountService, $window, $timeout) {
 
         $scope.doTransfer = function () {
             accountService.transfer($scope.amount, $scope.accountName).then(x => {
-                $scope.balance = x;
+                $rootScope.balance = x;
+                $rootScope.error = false;
                 console.log(x);
                 //$("#overlay").show();
-                $('.toast').toast('show');
+                $('.toast-ok').toast('show');
 
                 $timeout(function () { $window.location.href = 'https://localhost:44318/Account/Index'; }, 1000);
 
             }).catch(err => {
+                $rootScope.error = true;
+                $rootScope.errorMessage = err;
+                $('.toast').toast('show');
                 console.log(err);
             });
         };
     }
-    transferController.$inject = ['$scope', 'accountService', '$window', '$timeout'];
+    transferController.$inject = ['$rootScope','$scope', 'accountService', '$window', '$timeout'];
     angular.module('myApp').controller('transferController', transferController);
 
 }());
 
 (function () {
     "use strict";
-    function accountController($scope, accountService, $window, $timeout) {
+    function accountController($rootScope,$scope, accountService, $window, $timeout) {
 
         $scope.doSearch = function (page) {
             $("#overlay").show();
             accountService.search(page).then(x => {
                 console.log(x);
+                $rootScope.error = true;
                 $scope.items = x.result;
 
                 $scope.pageIndex = x.pageIndex;
@@ -215,6 +230,9 @@
 
                 $("#overlay").hide();
             }).catch(err => {
+                $rootScope.error = false;
+                $rootScope.errorMessage = err;
+                $('.toast').toast('show');
                 $scope.items = [];
                 console.log(err);
                 $("#overlay").hide();
@@ -225,7 +243,7 @@
 
         $scope.doSearch();
     }
-    accountController.$inject = ['$scope', 'accountService', '$window', '$timeout'];
+    accountController.$inject = ['$rootScope','$scope', 'accountService', '$window', '$timeout'];
     angular.module('myApp').controller('accountController', accountController);
 
 }());
