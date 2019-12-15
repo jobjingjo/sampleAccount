b4a5e7ebbb0fd5cbae3e1aeb6f1188dd3a3b0f78
Kaiwanyawit Chawankul <kaiwanyawit.c@draycir.com>
2019-12-15 20:27:01 +0700

    Fix external service

--- source/sampleAccount/Services/ExternalService.cs
+++ source/sampleAccount/Services/ExternalService.cs
@@ -5,15 +5,28 @@ using System.Threading.Tasks;
 
 namespace sampleAccount.Services
 {
-    public class ExternalService: IExternalService
+    public class ExternalService: IExternalService,IDisposable
     {
-        public async Task<string> GetIBAN()
-        {
+        bool disposed = false;
+        private Browser _browser;
+        public async Task<Browser> browserAsync() {
+            if (_browser != null) return _browser;
             await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
-            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            _browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = true
             });
+            return _browser;
+        }
+
+        public Browser browser {
+            get {
+                return browserAsync().Result;
+            }
+        }
+
+        public async Task<string> GetIBAN()
+        {
             var page = await browser.NewPageAsync();
             await page.GoToAsync("http://randomiban.com/?country=Netherlands");
             //<p id="demo" class="ibandisplay">NL71ABNA5985398153</p>
@@ -25,5 +38,33 @@ namespace sampleAccount.Services
             return innerHtml;
         }
 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                //
+                Task.Run(async () => await browser.CloseAsync()).Wait();    
+            }
+
+            // Free any unmanaged objects here.
+            //
+            disposed = true;
+        }
+
+        ~ExternalService()
+        {
+            Dispose(false);
+        }
     }
 }
